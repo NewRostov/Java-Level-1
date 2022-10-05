3998fb4b513f7107f74abd974f7e7d7797337fff
NewRostov <kim.sergey.rostov@gmail.com>
2022-10-06 01:36:47 +0200

    commit 3

---
+++ src/main/java/rostov/Lesson3.java
@@ -0,0 +1,7 @@
+package rostov;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        System.out.println("lesson3");
+    }
+}
--- src/main/java/rostov/lesson3.java
+++
@@ -1,4 +0,0 @@
-package rostov;
-
-public class lesson3 {
-}
